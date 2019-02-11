8a5d821eaef64a31d50ad09b87d2a13cbbd76f45
Administrator <877129310@qq.com>
2019-02-11 16:36:18 +0800

    南京V3.0

---
+++ aaa.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ! /usr/bin/env python
+
+"""
+@author:LiWei
+@license:LiWei
+@contact:877129310@qq.com
+@version:
+@var:
+@note:
+
+"""
+a = "500.123"
+n = a.rfind(".")
+print n
